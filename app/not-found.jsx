63267914bc678d5032b7ac797f41c24c74380383
Abismar <abismar.chintan@gmail.com>
2024-05-31 02:52:28 +0530

    It wouldn't deploy cuz it was in lowercase bruh

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-export default function notFound() {
+export default function NotFound() {
 	const router = useRouter();
 	let [seconds, setSeconds] = useState(3);
 	const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
